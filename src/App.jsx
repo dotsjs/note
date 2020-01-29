74ef751cd5e347f2bc74b2c238912bf795d67d17
ParkEunwoo <dmsdn960@gmail.com>
2020-01-30 00:56:56 +0900

    add note's function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: "01",
+      title: "제목1",
+      content: "내용1"
+    },
+    {
+      id: "02",
+      title: "제목2",
+      content: "내용2"
+    },
+    {
+      id: "03",
+      title: "제목3",
+      content: "내용3"
+    },
+    {
+      id: "04",
+      title: "제목4",
+      content: "내용4"
+    }
+  ]);
+
+  function addNote(title, content) {
+    const id = Date.now();
+    setNotes(prev => [...prev, { id, title, content }]);
+  }
+  function indexOfNote(id) {
+    return notes.findIndex(note => note.id === id);
+  }
+  function modifyNote(id, title, content) {
+    const index = indexOfNote(id);
+    setNotes(prev => [
+      ...prev.slice(0, index),
+      { id, title, content },
+      ...prev.slice(index + 1)
+    ]);
+  }
+  function deleteNote(id) {
+    const index = indexOfNote(id);
+    setNotes(prev => [...prev.slice(0, index), ...prev.slice(index + 1)]);
+  }
   return (
     <div className="App">
       <Header />
